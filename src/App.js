4969264c93c24bf788fc862920898e9792e54c1a
etztrefis <etztrefishello@gmail.com>
2020-11-24 12:55:03 +0700

    login and signup routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import {Route, Switch, BrowserRouter} from "react-router-dom";
-import HomePage from "./HomePage.js";
-import LoginPage from "./LoginPage.js";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./Pages/Home/Home.js";
+import AdminPage from "./Pages/Admin/Admin.js";
+import Login from "./Pages/Login/Login.js";
+import Signup from "./Pages/Signup/Signup.js";
+import { AuthContext } from "./context/auth.js";
+import PrivateRoute from "./Routes/PrivateRoute.js";
 
-import "./App.css"
+import "./App.css";
 
-class App extends React.Component{
-  render(){
-    return(
-      <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/login" component={LoginPage}/>
-          </Switch>
-      </BrowserRouter>
-    );
-  }
+function App(props) {
+	return (
+		<AuthContext.Provider value={false}>
+			<Router>
+				<div>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/login" component={Login} />
+					<Route path="/signup" component={Signup} />
+					<PrivateRoute path="/admin" component={AdminPage} />
+				</div>
+			</Router>
+		</AuthContext.Provider>
+	);
 }
 
 export default App;
