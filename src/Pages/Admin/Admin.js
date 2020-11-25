1835f519d9b97dd7fc19f77454167a1481229fba
etztrefis <etztrefishello@gmail.com>
2020-11-25 12:45:47 +0700

    admin page logout button, postlogin function

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Admin.css";
 import { Helmet } from "react-helmet";
+import { useAuth } from "../../context/auth.js";
+import { Button } from "../../Components/AuthForm/AuthForm.js";
 
 function Admin(props) {
+	const { setAuthTokens } = useAuth();
+
+	function logOut() {
+		setAuthTokens();
+	}
 	return (
 		<div className="App">
 			<Helmet>
-				<title>Join EateryBot</title>
+				<title>EateryBot | Dashboard</title>
 			</Helmet>
-			Kappa 123
+			<Button onClick={logOut}>Log out</Button>
 		</div>
 	);
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useAuth } from "../../context/auth.js";
+import axios from "axios";
 import "./Login.css";
 import logoImg from "../../img/logo.png";
 import {
@@ -12,14 +14,47 @@ import {
 	Button,
 	Text,
 	Header,
+	Error,
 } from "../../Components/AuthForm/AuthForm.js";
 
-function Login() {
+function Login(props) {
 	const eye = <FontAwesomeIcon icon={faEye} />;
 	const [passwordShown, setPasswordShown] = useState(false);
 	const togglePasswordVisiblity = () => {
 		setPasswordShown(passwordShown ? false : true);
 	};
+
+	const [isLoggedIn, setLoggedIn] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [userName, setUserName] = useState("");
+	const [password, setPassword] = useState("");
+	const { setAuthTokens } = useAuth();
+
+	function postLogin() {
+		axios
+			.post("https://www.eaterybot.trefis.net/login", {
+				userName,
+				password,
+			})
+			.then((result) => {
+				if (result.status === 200) {
+					setAuthTokens(result.data);
+					setLoggedIn(true);
+				} else {
+					setIsError(true);
+				}
+			})
+			.catch((e) => {
+				setIsError(true);
+			});
+	}
+
+	const referer = props.location.state.referer || "/";
+
+	if (isLoggedIn) {
+		return <Redirect to={referer} />;
+	}
+
 	return (
 		<div className="wrapper">
 			<div className="outer">
@@ -30,19 +65,32 @@ function Login() {
 							<Header>Вход</Header>
 							<Text>Перейти в панель администратора</Text>
 							<Form>
-								<Input type="email" placeholder="email" />
+								<Input
+									type="username"
+									value={userName}
+									onChange={(e) => {
+										setUserName(e.target.value);
+									}}
+									placeholder="email"
+								/>
 								<div className="pass-wrapper">
 									<Input
 										type={
 											passwordShown ? "text" : "password"
 										}
+										value={
+											passwordShown ? "text" : "password"
+										}
+										onChange={(e) => {
+											setPassword(e.target.value);
+										}}
 										placeholder="password"
 									/>
 									<i onClick={togglePasswordVisiblity}>
 										{eye}
 									</i>{" "}
 								</div>
-								<Button>Войти</Button>
+								<Button onClick={postLogin}>Войти</Button>
 							</Form>
 							<Link
 								to="/signup"
@@ -50,6 +98,9 @@ function Login() {
 							>
 								Первый раз на нашем сайте?
 							</Link>
+							{isError && (
+								<Error>Почта или пароль указаны неверно.</Error>
+							)}
 						</Card>
 					</div>
 				</div>
