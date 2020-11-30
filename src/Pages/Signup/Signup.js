348c1d37292a357a1b3ffdc4103fb63683d42ff7
etztrefis <etztrefishello@gmail.com>
2020-11-30 11:59:59 +0700

    validate password and email functions, check if passwords are the same, handling errors

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -11,6 +11,7 @@ import {
 	Button,
 	Text,
 	Header,
+	Error,
 } from "../../Components/AuthForm/AuthForm.js";
 
 import "./Signup.css";
@@ -23,9 +24,53 @@ function Signup() {
 	};
 
 	const [isError, setIsError] = useState(false);
+	const [isPasswordTheSameError, setIsPasswordTheSameError] = useState(false);
+	const [isEmailValidateError, setIsEmailValidateError] = useState(false);
+	const [isPasswordValidateError, setIsPasswordValidateError] = useState(
+		false
+	);
 	const [userName, setUserName] = useState("");
 	const [password, setPassword] = useState("");
 	const [passwordAgain, setPasswordAgain] = useState("");
+
+	const isPasswordTheSame = () => {
+		if (password === passwordAgain && passwordAgain !== "") {
+			setIsPasswordTheSameError(false);
+			return false;
+		} else {
+			setIsPasswordTheSameError(true);
+			return true;
+		}
+	};
+
+	const validateEmail = () => {
+		const reg = new RegExp(
+			//eslint-disable-next-line
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
+		const result = userName.match(reg);
+		if (result === null && userName !== "") {
+			setIsEmailValidateError(true);
+			return false;
+		} else {
+			setIsEmailValidateError(false);
+			return true;
+		}
+	};
+
+	const validatePassword = () => {
+		const reg = new RegExp(
+			/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+		);
+		const result = password.match(reg);
+		if (result === null && password !== "") {
+			setIsPasswordValidateError(true);
+			return false;
+		} else {
+			setIsPasswordValidateError(false);
+			return true;
+		}
+	};
 
 	// fucntion postSignup(){
 	// }
@@ -47,7 +92,11 @@ function Signup() {
 									type="email"
 									placeholder="email"
 									value={userName}
-									autoFocus="true"
+									autoFocus={true}
+									onChange={(e) => {
+										setUserName(e.target.value);
+									}}
+									onBlur={validateEmail}
 								/>
 								<div className="pass-wrapper">
 									{" "}
@@ -57,6 +106,10 @@ function Signup() {
 										}
 										value={password}
 										placeholder="password"
+										onChange={(e) => {
+											setPassword(e.target.value);
+										}}
+										onBlur={validatePassword}
 									/>
 									<i onClick={togglePasswordVisiblity}>
 										{eye}
@@ -66,12 +119,42 @@ function Signup() {
 									type={passwordShown ? "text" : "password"}
 									value={passwordAgain}
 									placeholder="password again"
+									onChange={(e) => {
+										setPasswordAgain(e.target.value);
+									}}
+									onBlur={isPasswordTheSame}
 								/>
 								<Button>Зарегестрироваться</Button>
 							</Form>
 							<Link to="/login" style={{ paddingBottom: "10px" }}>
 								Уже есть аккаунт?
 							</Link>
+							{isPasswordTheSameError && (
+								<div className="error-wrapper">
+									<Error>Пароли не совпадают.</Error>
+								</div>
+							)}
+							{isEmailValidateError && (
+								<div className="error-wrapper">
+									<Error>Такой email не существует.</Error>
+								</div>
+							)}
+							{isPasswordValidateError && (
+								<div className="error-wrapper">
+									<Error>
+										Минимум 8 символов.
+										<br />
+										Хотя бы одна цифра.
+										<br />
+										Хотя бы один специальный символ.
+										<br />
+										Хотя бы один символ в верхнем регистре.
+										<br />
+										Хотя бы один символ в нижнем регистре.
+										<br />
+									</Error>
+								</div>
+							)}
 							<Link to="/" style={{ paddingBottom: "15px" }}>
 								Назад
 							</Link>
