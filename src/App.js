48c5e7f214d9438f070b6a5ab6e1b64405fa7703
etztrefis <etztrefishello@gmail.com>
2020-12-18 12:20:47 +0700

    admin/* private routes and styles for them

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import HomePage from "./Pages/Home/Home.js";
 import AdminPage from "./Pages/Admin/Admin.js";
 import Login from "./Pages/Login/Login.js";
 import Signup from "./Pages/Signup/Signup.js";
+import Devices from "./Pages/Admin/Devices/Devices";
+import Tests from "./Pages/Admin/Test/Test.js";
+import Home from "./Pages/Admin/Home/Home.js";
 import { AuthContext } from "./context/auth.js";
 import PrivateRoute from "./Routes/PrivateRoute.js";
 
@@ -24,7 +27,10 @@ function App(props) {
 					<Route exact path="/" component={HomePage} />
 					<Route path="/login" component={Login} />
 					<Route path="/signup" component={Signup} />
-					<PrivateRoute path="/admin" component={AdminPage} />
+					<PrivateRoute path="/admin" exact component={AdminPage} />
+					<PrivateRoute path="/admin/home" component={Home} />
+					<PrivateRoute path="/admin/devices" component={Devices} />
+					<PrivateRoute path="/admin/test" component={Tests} />
 				</div>
 			</Router>
 		</AuthContext.Provider>
