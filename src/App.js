bf30d69edb9c67e54747d3045603353b25e8372c
etztrefis <etztrefishello@gmail.com>
2020-12-25 10:09:57 +0700

    removed routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import HomePage from "./Pages/Home/Home.js";
 import AdminPage from "./Pages/Admin/Admin.js";
 import Login from "./Pages/Login/Login.js";
 import Signup from "./Pages/Signup/Signup.js";
-import Devices from "./Pages/Admin/Devices/Devices";
-import Tests from "./Pages/Admin/Test/Test.js";
-import Home from "./Pages/Admin/Home/Home.js";
 import { AuthContext } from "./context/auth.js";
 import PrivateRoute from "./Routes/PrivateRoute.js";
 
@@ -27,10 +24,7 @@ function App(props) {
 					<Route exact path="/" component={HomePage} />
 					<Route path="/login" component={Login} />
 					<Route path="/signup" component={Signup} />
-					<PrivateRoute path="/admin" exact component={AdminPage} />
-					<PrivateRoute path="/admin/home" component={Home} />
-					<PrivateRoute path="/admin/devices" component={Devices} />
-					<PrivateRoute path="/admin/test" component={Tests} />
+					<PrivateRoute path="/admin/" component={AdminPage} />
 				</div>
 			</Router>
 		</AuthContext.Provider>
