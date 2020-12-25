eeec1ebf5d0f55edf0ec1dbdc4b9daec72c7f9ca
etztrefis <etztrefishello@gmail.com>
2020-12-25 10:14:25 +0700

    rework sidebar component with new styles

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,46 +1,105 @@
-import React, { useState } from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import { Link } from "react-router-dom";
-import "./Sidebar.css";
-import { SidebarData } from "./SidebarData";
-import { IconContext } from "react-icons";
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import "../../Pages/Admin/Admin.css";
 
-function Sidebar() {
-	const [sideBar, setSideBar] = useState(false);
+const useStyles = makeStyles({
+	list: {
+		width: 250,
+	},
+	fullList: {
+		width: "auto",
+	},
+});
 
-	const showSideBar = () => setSideBar(!sideBar);
+export default function Sidebar() {
+	const classes = useStyles();
+	const [state, setState] = React.useState({
+		top: false,
+		left: false,
+		bottom: false,
+		right: false,
+	});
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (
+			event &&
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+
+		setState({ ...state, [anchor]: open });
+	};
+
+	const list = (anchor) => (
+		<div
+			className={clsx(classes.list, {
+				[classes.fullList]: anchor === "top" || anchor === "bottom",
+			})}
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				{["Inbox", "Starred", "Send email", "Drafts"].map(
+					(text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>
+								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					)
+				)}
+			</List>
+			<Divider />
+			<List>
+				{["All mail", "Trash", "Spam"].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>
+							{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+						</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
 
 	return (
-		<>
-			<IconContext.Provider value={{ color: "#fff" }}>
-				<div className="sidebar">
-					<Link to="/admin" className="menu-bars">
-						<FaIcons.FaBars onClick={showSideBar} />
-					</Link>
-				</div>
-				<nav className={sideBar ? "nav-menu active" : "nav-menu"}>
-					<ul className="nav-menu-items" onClick={showSideBar}>
-						<li className="navbar-toggle">
-							<Link to="/admin" className="menu-bars">
-								<AiIcons.AiOutlineClose />
-							</Link>
-						</li>
-						{SidebarData.map((item, index) => {
-							return (
-								<li key={index} className={item.cName}>
-									<Link to={item.path}>
-										{item.icon}
-										<span>{item.title}</span>
-									</Link>
-								</li>
-							);
-						})}
-					</ul>
-				</nav>
-			</IconContext.Provider>
-		</>
+		<div className="sidebar-top-wrapper">
+			{["left"].map(() => (
+				<React.Fragment key={"left"}>
+					<Button
+						onClick={toggleDrawer("left", true)}
+						style={{
+							color: "white",
+						}}
+					>
+						{<MenuIcon />}
+					</Button>
+					<SwipeableDrawer
+						anchor={"left"}
+						open={state["left"]}
+						onClose={toggleDrawer("left", false)}
+						onOpen={toggleDrawer("left", true)}
+					>
+						{list("left")}
+					</SwipeableDrawer>
+				</React.Fragment>
+			))}
+		</div>
 	);
 }
-
-export default Sidebar;
