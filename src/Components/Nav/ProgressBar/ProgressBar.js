1e4fc106321291b5b36aa6560d488b93f1e1c249
etztrefis <etztrefishello@gmail.com>
2021-01-11 18:00:14 +0700

    changed main color of progressbar and z-index on navbar

--- src/Components/Nav/ProgressBar/ProgressBar.js
+++ src/Components/Nav/ProgressBar/ProgressBar.js
@@ -1,49 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ProgressBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scroll: 0
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			scroll: 0,
+		};
+	}
 
-    progress = () => {
-        const scrollTotal = document.documentElement.scrollTop;
-        const heightWin = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        const scroll = `${scrollTotal / heightWin * 100}%`
+	progress = () => {
+		const scrollTotal = document.documentElement.scrollTop;
+		const heightWin =
+			document.documentElement.scrollHeight -
+			document.documentElement.clientHeight;
+		const scroll = `${(scrollTotal / heightWin) * 100}%`;
 
-        this.setState({
-            scroll: scroll,
-        });
-    };
+		this.setState({
+			scroll: scroll,
+		});
+	};
 
-    componentDidMount() {
-        window.addEventListener("scroll", this.progress)
-    }
-    componentWillUnmount() {
-        window.addEventListener("scroll", this.progress)
-    }
+	componentDidMount() {
+		window.addEventListener("scroll", this.progress);
+	}
+	componentWillUnmount() {
+		window.addEventListener("scroll", this.progress);
+	}
 
-    render() {
-        const progressWrapper = {
-            backdropFilter: "blur(5px)",
-            backgroundColor: "#0001",
-            height: "5px",
-            zIndex: 101,
-            width: "100%"
-        };
+	render() {
+		const progressWrapper = {
+			backdropFilter: "blur(5px)",
+			backgroundColor: "#0001",
+			height: "5px",
+			zIndex: 101,
+			width: "100%",
+		};
 
-        const progressStyle = {
-            height: "5px",
-            background: "yellow",
-            width: this.state.scroll,
-        };
+		const progressStyle = {
+			height: "5px",
+			background: "#8E0C31",
+			width: this.state.scroll,
+		};
 
-        return (
-            <div className="progress-bar" style={progressWrapper} >
-                <div style={progressStyle} />
-            </div>
-        )
-    }
+		return (
+			<div className="progress-bar" style={progressWrapper}>
+				<div style={progressStyle} />
+			</div>
+		);
+	}
 }
