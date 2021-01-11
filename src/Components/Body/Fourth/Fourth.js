59ba8e8e516e3dca304c10972ed9905663796718
etztrefis <etztrefishello@gmail.com>
2021-01-11 21:21:49 +0700

    fourth body component

---
+++ src/Components/Body/Fourth/Fourth.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import "./Fourth.css";
+
+function Fourth() {
+	return (
+		<div className="fourth-container">
+			<div className="triangle-fourth-topright"></div>
+			<div className="triangle-fourth-topleft"></div>
+		</div>
+	);
+}
+
+export default Fourth;
