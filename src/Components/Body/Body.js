c9cebc57cbf2b1369dc54e6683d238b22c575251
etztrefis <etztrefishello@gmail.com>
2021-01-11 21:29:06 +0700

    fourth component added to body

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -2,6 +2,7 @@ import React from "react";
 import First from "./First/First.js";
 import Second from "./Second/Second.js";
 import Third from "./Third/Third.js";
+import Fourth from "./Fourth/Fourth.js";
 
 function Body() {
 	return (
@@ -13,6 +14,7 @@ function Body() {
 				<Second />
 			</div>
 			<Third />
+			<Fourth />
 		</div>
 	);
 }
