44ba054c2a5caaaf094b75400a06b7222fdb8dea
etztrefis <etztrefishello@gmail.com>
2021-01-11 18:02:00 +0700

    new anchor link in middle

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,17 +1,20 @@
 import React from "react";
-import First from "./First/First.js"
-import Second from "./Second/Second.js"
-import Third from "./Third/Third.js"
-
+import First from "./First/First.js";
+import Second from "./Second/Second.js";
+import Third from "./Third/Third.js";
 
 function Body() {
-    return (
-        <div>
-            <div id="home"><First /></div>
-            <Second />
-            <Third />
-        </div >
-    );
+	return (
+		<div>
+			<div id="home">
+				<First />
+			</div>
+			<div id="middle">
+				<Second />
+			</div>
+			<Third />
+		</div>
+	);
 }
 
 export default Body;
