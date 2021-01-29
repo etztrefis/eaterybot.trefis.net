ff8cc6163fb55c9693f2f38df919979f2b9a956d
etztrefis <etztrefishello@gmail.com>
2021-01-29 11:08:30 +0700

    main admin page reworked into react class and added admin layout for resposive grid

---
+++ src/Pages/Admin/AdminLayout.js
@@ -0,0 +1,111 @@
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+export default class AdminLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentBreakpoint: "lg",
+      compactType: "vertical",
+      mounted: false,
+      layouts: { lg: props.initialLayout },
+    };
+
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
+    this.onCompactTypeChange = this.onCompactTypeChange.bind(this);
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+    this.onNewLayout = this.onNewLayout.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts.lg, function (l, i) {
+      return (
+        <div key={i} className={l.static ? "static" : ""}>
+          <span className="text">{i}</span>
+        </div>
+      );
+    });
+  }
+
+  onBreakpointChange(breakpoint) {
+    this.setState({
+      currentBreakpoint: breakpoint,
+    });
+  }
+
+  onCompactTypeChange() {
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  }
+
+  onLayoutChange(layout, layouts) {
+    this.props.onLayoutChange(layout, layouts);
+  }
+
+  onNewLayout() {
+    this.setState({
+      layouts: { lg: getLayout() },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
+
+AdminLayout.propTypes = {
+  onLayoutChange: PropTypes.func.isRequired,
+};
+
+AdminLayout.defaultProps = {
+  className: "layout",
+  rowHeight: 30,
+  onLayoutChange: function () {},
+  cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  initialLayout: getLayout(),
+};
+
+function getLayout() {
+  return _.map(_.range(0, 10), function (item, i) {
+    var y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: (_.random(0, 5) * 2) % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: false,
+    };
+  });
+}
