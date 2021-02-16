f9384322f7e7860c01e7608873efa1168c435ac6
etztrefis <etztrefishello@gmail.com>
2021-02-16 10:00:14 +0700

    admin grid layout

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,30 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Sidebar from "../../Components/Sidebar/Sidebar";
-import AdminLayout from "./AdminLayout";
-import "./Admin.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import AdminLayout from './AdminLayout';
+import './Admin.css';
 
 export default class AdminPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { layout: [] };
-    this.onLayoutChange = this.onLayoutChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {layout: []};
+        this.onLayoutChange = this.onLayoutChange.bind(this);
+    }
 
-  onLayoutChange(layout) {
-    this.setState({ layout: layout });
-  }
+    onLayoutChange(layout) {
+        this.setState({layout: layout});
+    }
 
-  render() {
-    return (
-      <div className="adminpage-main">
-        <Sidebar />
-        <AdminLayout onLayoutChange={this.onLayoutChange} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="adminpage-main">
+                <Sidebar />
+                <AdminLayout onLayoutChange={this.onLayoutChange} />
+            </div>
+        );
+    }
 }
 
-const contentDiv = document.getElementById("root");
+const contentDiv = document.getElementById('root');
 const gridProps = window.gridProps || {};
 ReactDOM.render(React.createElement(AdminLayout, gridProps), contentDiv);
