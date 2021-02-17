f3b8555119b802c8d6977707ac4ddddb6ea99aa5
etztrefis <etztrefishello@gmail.com>
2021-02-17 23:47:23 +0700

    localization for table, onRowAdd, onRowUpdate, onRowDelete

--- src/Pages/Admin/Products/ProductsLayout.js
+++ src/Pages/Admin/Products/ProductsLayout.js
@@ -7,35 +7,34 @@ const tableRef = React.createRef();
 import axios from 'axios';
 
 export default class ResponsiveLocalStorageLayout extends React.PureComponent {
+    _isMounted = false;
+
     constructor(props) {
         super(props);
 
         this.state = {
             layouts: JSON.parse(JSON.stringify(originalLayouts)),
             data: [],
-            isLoaded: false
         };
     }
 
-    componentDidMount() {
-        let resp = this.getProducts({headers: {Authorization : `Bearer ${process.env.REACT_APP_API_KEY}`}});
-        this.setState({
-            isLoaded: true,
-            data: resp
-        })
-        console.log(this.state.data);
+    async componentDidMount() {
+        this._isMounted = true;
+        const server = `${process.env.REACT_APP_API_SERVER}products/`;
+        await axios.get(server, { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+            .then(response => {
+                if (this._isMounted) {
+                    this.setState({
+                        data: response.data.message
+                    })
+                }
+            }).catch(error => {
+                console.log(error);
+            })
     }
 
-    getProducts = this.getProductsAsync;
-
-    async getProductsAsync(conf) {
-        await axios.get('http://92.168.0.87:8082/api/products/', conf)
-            .then(response => {
-                return response;
-            })
-            .catch(error => {
-                console.error(error);
-            });
+    componentWillUnmount() {
+        this._isMounted = false;
     }
 
     static get defaultProps() {
@@ -67,42 +66,119 @@ export default class ResponsiveLocalStorageLayout extends React.PureComponent {
                         this.onLayoutChange(layout, layouts)
                     }
                 >
-                    <div key="1" data-grid={{ w: 6, h: 8, x: 0, y: 0 }}>
-                        <span className="text">
-                            Добавить новый продукт
-                        </span>
-                    </div>
-                    <div key="2" data-grid={{ w: 6, h: 8, x: 0, y: 1 }}>
+                    <div key="2" data-grid={{ w: 5, h: 8, x: 0, y: 0 }}>
                         <span className="text">Логи</span>
                     </div>
-                    <div key="3" data-grid={{ w: 6, h: 20, x: 6, y: 0, static: true }}>
+                    <div key="3" data-grid={{ w: 7, h: 18, x: 5, y: 0, static: true }}>
                         <span className="text">
                             <MaterialTable
                                 title="Продукты"
                                 tableRef={tableRef}
+                                data={this.state.data}
                                 columns={[
                                     { title: 'ID', field: 'ProductID' },
                                     { title: 'Имя', field: 'Name' },
                                     { title: 'Количество', field: 'Amount', type: 'numeric' },
-                                    { title: 'Единица измерения', field: 'MeasurmentUnits' },
+                                    { title: 'Единица измерения', field: 'MeasurmentUnits', lookup: { "шт.": "шт.", "кг.": "кг.", "литр.": "литр." } },
                                 ]}
-                                // data={this.state.data}
                                 options={{
-                                    selection: true,
+                                    paging: true,
+                                    pageSize: 15,
+                                    emptyRowsWhenPaging: false,
+                                    pageSizeOptions: [15, 30, 60, 90],
                                 }}
-                                actions={[
-                                    {
-                                        icon: 'refresh',
-                                        tooltip: 'Обновить записи',
-                                        isFreeAction: true,
-                                        onClick: () => tableRef.current && tableRef.current.onQueryChange(),
+                                localization={{
+                                    header: {
+                                        actions: "Действия"
                                     },
-                                    {
-                                        tooltip: 'Удалить выбранные записи',
-                                        icon: 'delete',
-                                        onClick: (event, data) => alert('You want to delete ' + data.length + ' rows'),
+                                    toolbar: {
+                                        searchPlaceholder: "Поиск",
+                                        searchTooltip: "Поиск",
+                                        nRowsSelected: "{0} строк(и) выбрано",
                                     },
-                                ]}
+                                    pagination: {
+                                        labelDisplayedRows: "{from}-{to} из {count}",
+                                        labelRowsSelect: "строк",
+                                        firstTooltip: "Первая страница",
+                                        previousTooltip: "Предыдущая страница",
+                                        nextTooltip: "Следующая страница",
+                                        lastTooltip: "Последняя страница"
+                                    },
+                                    body: {
+                                        addTooltip: "Новая запись",
+                                        deleteTooltip: "Удалить",
+                                        editTooltip: "Изменить",
+                                        editRow: {
+                                            deleteText: "Вы уверены, что хотите удалить эту запись?",
+                                            saveTooltip: "Сохранить",
+                                            cancelTooltip: "Отмена"
+                                        }
+                                    }
+                                }}
+                                editable={{
+                                    onRowAdd: async (newData) => {
+                                        if (newData.Name !== undefined && newData.Amount !== undefined && newData.MeasurmentUnits !== undefined) {
+                                            const server = `${process.env.REACT_APP_API_SERVER}products/create/${newData.Name}/${newData.Amount}/${newData.MeasurmentUnits}`;
+                                            await axios.get(server, { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+                                                .then(response => {
+                                                    if (response.status == 200) {
+                                                        this.componentDidMount();
+                                                    } else {
+                                                        console.error('404 error ' + response);
+                                                    }
+                                                })
+                                                .catch(error => {
+                                                    console.error(error);
+                                                })
+                                        } else {
+                                            alert("Для создания новой записи необходимо заполнить все поля.");
+                                        }
+                                        new Promise((resolve, reject) => {
+                                            setTimeout(() => {
+                                                resolve()
+                                            }, 1000)
+                                        })
+                                    },
+                                    onRowUpdate: async (newData, oldData) => {
+                                        const server = `${process.env.REACT_APP_API_SERVER}products/update/${newData.ProductID}/${newData.Name}/${newData.Amount}/${newData.MeasurmentUnits}`;
+                                        await axios.get(server, { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+                                            .then(response => {
+                                                if (response.status == 200) {
+                                                    this.componentDidMount();
+                                                } else {
+                                                    console.error('404 error ' + response);
+                                                }
+                                            })
+                                            .catch(error => {
+                                                console.error(error);
+                                            })
+                                        new Promise((resolve, reject) => {
+                                            setTimeout(() => {
+                                                resolve()
+                                            }, 1000)
+                                        })
+                                    },
+                                    onRowDelete: async (oldData) => {
+                                        const server = `${process.env.REACT_APP_API_SERVER}products/delete/${oldData.ProductID}`;
+                                        await axios.get(server, { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } })
+                                            .then(response => {
+                                                if (response.status == 200) {
+                                                    this.componentDidMount();
+                                                } else {
+                                                    console.error('404 error ' + response);
+                                                }
+                                            })
+                                            .catch(error => {
+                                                console.error(error);
+                                            })
+                                        new Promise((resolve, reject) => {
+                                            setTimeout(() => {
+                                                resolve()
+                                            }, 1000)
+                                        })
+                                    }
+                                }
+                                }
                             />
                         </span>
                     </div>
