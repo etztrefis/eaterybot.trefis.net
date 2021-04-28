ed381e9246865ebb259f580ef6fc18aa5e05bf01
etztrefis <etztrefishello@gmail.com>
2021-04-28 21:33:13 +0700

    :art: theme customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,18 @@ module.exports = {
       red: colors.rose,
       purple: colors.purple,
       white: colors.white,
+      green: colors.green,
+    },
+    boxShadow: {
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
