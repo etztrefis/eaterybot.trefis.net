354f88be6703e9ae64e83c3e1ef3a429abe0bb96
etztrefis <etztrefishello@gmail.com>
2021-04-28 22:57:23 +0700

    now we can revice messages from people through site form

--- src/Components/Body/BodyComponent/BodyComponent.js
+++ src/Components/Body/BodyComponent/BodyComponent.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import poster from "../../../assets/poster.png";
 import statsImg from "../../../assets/chat.png";
 import menuVideo from "../../../assets/video/menu.mp4";
@@ -7,6 +8,25 @@ import commsVideo from "../../../assets/video/comms.mp4";
 import otherVideo from "../../../assets/video/other.mp4";
 
 export default function BodyComponent() {
+	let [name, setName] = useState("");
+	let [email, setEmail] = useState("");
+	let [message, setMessage] = useState("");
+	const [isSent, setMessageSent] = useState(false);
+	async function messageSend() {
+		name = name === "" ? "No name provided" : name;
+		email = email === "" ? "No email provided" : email;
+		message = message === "" ? "No message provided" : message;
+		try {
+			const server = `${process.env.REACT_APP_API_SERVER}`;
+			await axios.get(`${server}message/${name}/${email}/${message}`);
+		} catch (error) {
+			console.error(error);
+		}
+		finally {
+			setMessageSent(true);
+		}
+	}
+
 	return (
 		<>
 			<section className="text-gray-600 body-font pb-12">
@@ -195,30 +215,38 @@ export default function BodyComponent() {
 									<div className="p-2 w-1/2">
 										<div className="relative">
 											<label htmlFor="name" className="leading-7 text-base text-gray-600">Имя</label>
-											<input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+											<input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onBlur={(e) => { setName(e.target.value); }} />
 										</div>
 									</div>
 									<div className="p-2 w-1/2">
 										<div className="relative">
 											<label htmlFor="email" className="leading-7 text-base text-gray-600">E-mail</label>
-											<input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+											<input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onBlur={(e) => { setEmail(e.target.value); }} />
 										</div>
 									</div>
 									<div className="p-2 w-full">
 										<div className="relative">
 											<label htmlFor="message" className="leading-7 text-base text-gray-600">Сообщение</label>
-											<textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+											<textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" onBlur={(e) => { setMessage(e.target.value); }}></textarea>
 										</div>
 									</div>
 									<div className="p-6 w-full">
-										<button className="flex mx-auto text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg shadow-lg transition duration-500">ОТПРАВИТЬ СООБЩЕНИЕ</button>
+										<button className="flex mx-auto text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg shadow-lg transition duration-500" onClick={messageSend}>ОТПРАВИТЬ СООБЩЕНИЕ</button>
 									</div>
 								</div>
 							</div>
+							{isSent && (
+								<div class="bg-indigo-900 text-center py-4 lg:px-4">
+									<div class="p-5 bg-purple-300 items-center text-gray-700 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+										<span class="font-semibold mr-2 text-left flex-auto">Ваше сообщение было оправлено!</span>
+										<svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" /></svg>
+									</div>
+								</div>
+							)}
 						</div>
 					</div>
 					<div className="md:w-1/2 md:pl-12 md:py-8 md:border-l md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-purple-400 border-opacity-90 mt-6">
-						<img className="object-cover object-center rounded-2xl" draggable="false" alt="hero" src={statsImg}/>
+						<img className="object-cover object-center rounded-2xl" draggable="false" alt="hero" src={statsImg} />
 					</div>
 				</div>
 			</section>
